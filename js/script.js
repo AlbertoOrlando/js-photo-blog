0f87a3a27c527a044adace3d529bc63b784697ac
AlbertoOrlando <albertoorlandowork@gmail.com>
2025-01-26 17:48:23 +0100

    [feat] esercizio prima parte

--- js/script.js
+++ js/script.js
@@ -4,18 +4,19 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/")
     .then(response => {
         const cards = response.data;
         for(let i = 0; i < cards.length; i++){
-            const card = cards[i];
+            card = cards[i];
             const { title, date, url } = card;
             contCards.innerHTML += `
-            <div class="col-4">
                 <div class="card mb-3">
                     <img src="${url}" class="card-img-top" alt="${title}">
                     <div class="card-body"> 
                         <p class="card-text"><small class="text-body-secondary">${date}</small></p>
                         <h5 class="card-title">${title}</h5>
                     </div>
+                        <div class="pin">
+                            <img src="img/pin.svg" alt="pin">
+                        </div>
                   </div>
-            </div>
             `;
         }
     })
