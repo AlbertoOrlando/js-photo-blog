25f3d196b225a505f7c0dda09b2fda04d9415951
AlbertoOrlando <albertoorlandowork@gmail.com>
2025-01-27 15:04:41 +0100

    patch posizione pin

--- js/script.js
+++ js/script.js
@@ -11,14 +11,12 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/")
             const { title, date, url } = card;
             contCards.innerHTML += `
                 <div class="card mb-3">
+                    <img class="pin" src="img/pin.svg" alt="pin">
                     <img src="${url}" class="card-img" alt="${title}">
                     <div class="card-body"> 
                         <p class="card-text"><small class="text-body-secondary">${date}</small></p>
                         <h5 class="card-title">${title}</h5>
                     </div>
-                        <div class="pin">
-                            <img src="img/pin.svg" alt="pin">
-                        </div>
                   </div>
             `;
         }
